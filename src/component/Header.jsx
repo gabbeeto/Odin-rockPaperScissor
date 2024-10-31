7cfafea948582828da99b90bf1721c0d15322e63
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2024-10-31 16:00:36 -0300

    set up the project

---
+++ src/component/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header() {
+	return (<header>
+		<h1 className="">rock paper scissor</h1>
+	</header>)
+}
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Header from './component/Header.jsx'
+
+createRoot(document.getElementById('root')).render(<>
+
+	< Header />
+
+	</>
+)
+
+
