7a2f1ec2db6d7ba26a247a4b1ba424b90f1c22ee
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2024-11-02 12:54:08 -0300

    make it so you can use blender camera

--- src/component/main/Game.jsx
+++ src/component/main/Game.jsx
@@ -1,19 +1,21 @@
 import {useEffect} from 'react'
 import * as three from 'three'
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
-import stage from './../../objectsIn3D/stage2.glb'
+import stage from './../../objectsIn3D/stage.glb'
 import {degToRad} from 'three/src/math/MathUtils';
 
 
 let sceneRender;
 
-const scene = new three.Scene()
-const camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.0001, 100000)
+const scene = new three.Scene();
+let camera
+// = 
+//  new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.0001, 100000)
 
-camera.position.x = -9;
-camera.position.z = 1;
-camera.position.y = 5;
-camera.rotateY(degToRad(-90))
+// camera.position.x = -9;
+// camera.position.z = 1;
+// camera.position.y = 5;
+// camera.rotateY(degToRad(-90))
 
 // scene.add(new three.Mesh(
 // 	new three.BoxGeometry(5, 5),
@@ -29,12 +31,16 @@ camera.rotateY(degToRad(-90))
 
 const objectLoader = new GLTFLoader();
 
-console.log(objectLoader)
-console.log()
+// console.log(objectLoader)
+// console.log()
 
 function animate() {
+	if(scene.children[1]){
+		camera = scene.children[1].children[1]
 
-	sceneRender.render(scene, camera);
+		sceneRender.render(scene, camera);
+	}
+
 }
 
 function load3DScene() {
@@ -47,17 +53,16 @@ function load3DScene() {
 	// console.log(sceneRender)
 
 	// scene.add(new three.AxesHelper())
-	// scene.add(new three.AmbientLight())
+	scene.add(new three.AmbientLight())
 	loadStageFile()
 
-	// Object3D.lightOff = true
-	sceneRender.setAnimationLoop(animate)
+		sceneRender.setAnimationLoop(animate)
 }
 
 
 function loadStageFile() {
 	objectLoader.load(stage, (file) => {
-		console.log(file.scene)
+		// console.log(file.scene)
 
 		scene.add(file.scene)
 	})
