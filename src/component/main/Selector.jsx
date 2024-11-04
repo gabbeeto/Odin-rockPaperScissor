eec363822e7dee5dd0e70559daffa19faf6ee7fb
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2024-11-04 14:32:33 -0300

    make it look cooler

--- src/component/main/Selector.jsx
+++ src/component/main/Selector.jsx
@@ -9,14 +9,22 @@ import Displayer from "../display3DObjects/DisplayerFor3D.jsx"
 
 
 export default function Selector() {
-	return <section className='flex justify-center items-center'>
+	const selectorStyle = 'bg-orange-500  rounded-full ';
 
-		<Displayer stage={Paper} widthAndHeightArray={[window.innerWidth * 0.3, window.innerHeight * .3]} extraFunctionality={object2 => { object2.rotation.y += .01}} idForObject='paper' />
+	return <section className='flex justify-center flex-col items-center gap-2 border-orange-400 border-2 p-4 rounded-lg m-4'>
 
-		<Displayer stage={Rock} widthAndHeightArray={[window.innerWidth * 0.3, window.innerHeight * .3]} extraFunctionality={object => { object.rotation.y += .01}} idForObject='rock' />
-
-		<Displayer stage={Scissor} widthAndHeightArray={[window.innerWidth * 0.3, window.innerHeight * .3]} extraFunctionality={object => { object.rotation.y += .01}} idForObject='scissor' />
-
+		<h2 className='text-orange-400 bg-orange-400 text-black p-4 rounded-full'>select one</h2>
+		<div className='flex justify-center gap-4 items-center '>
+			<div className={selectorStyle}>
+				<Displayer stage={Paper} widthAndHeightArray={[window.innerWidth * 0.3, window.innerHeight * .3]} extraFunctionality={object2 => {object2.rotation.y += .01}} idForObject='paper' />
+			</div>
+			<div className={selectorStyle}>
+				<Displayer stage={Rock} widthAndHeightArray={[window.innerWidth * 0.3, window.innerHeight * .3]} extraFunctionality={object => {object.rotation.y += .01}} idForObject='rock' />
+			</div>
+			<div className={selectorStyle}>
+				<Displayer stage={Scissor} widthAndHeightArray={[window.innerWidth * 0.3, window.innerHeight * .3]} extraFunctionality={object => {object.rotation.y += .01}} idForObject='scissor' />
+			</div>
+		</div>
 	</section>
 
 }
