efbb100b23c4eb5f3ab65c7620a3efde7804b3b2
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2024-11-04 15:07:35 -0300

    add some good looking hover

--- src/component/main/Selector.jsx
+++ src/component/main/Selector.jsx
@@ -6,24 +6,26 @@ import Scissor from './../../objectsIn3D/scissor.glb'
 import Displayer from "../display3DObjects/DisplayerFor3D.jsx"
 
 
+function ItemChoice({stage, id}) {
+
+	const selectorStyle = 'bg-orange-500 p-4 hover:bg-amber-400  rounded-lg ';
+	let directionForItem = 1;
+
+	return (<div onMouseEnter={() => {directionForItem = -5}} onMouseLeave={() => {directionForItem = 1}} className={selectorStyle} >
+		<Displayer stage={stage} widthAndHeightArray={[window.innerWidth * 0.25, window.innerHeight * .25]} extraFunctionality={object2 => {object2.rotation.y += .01 * directionForItem}} idForObject={id} />
+	</div>)
+}
 
 
 export default function Selector() {
-	const selectorStyle = 'bg-orange-500  rounded-full ';
+	const selectorStyle = 'bg-orange-500 p-4 hover:bg-amber-400  rounded-lg ';
+	return <section className='flex justify-center flex-col items-center gap-2 border-orange-400 border-2 p-4 rounded-lg m-4 '>
 
-	return <section className='flex justify-center flex-col items-center gap-2 border-orange-400 border-2 p-4 rounded-lg m-4'>
-
-		<h2 className='text-orange-400 bg-orange-400 text-black p-4 rounded-full'>select one</h2>
+		<h2 className=' bg-orange-400  text-black p-4 rounded-lg'>select one</h2>
 		<div className='flex justify-center gap-4 items-center '>
-			<div className={selectorStyle}>
-				<Displayer stage={Paper} widthAndHeightArray={[window.innerWidth * 0.3, window.innerHeight * .3]} extraFunctionality={object2 => {object2.rotation.y += .01}} idForObject='paper' />
-			</div>
-			<div className={selectorStyle}>
-				<Displayer stage={Rock} widthAndHeightArray={[window.innerWidth * 0.3, window.innerHeight * .3]} extraFunctionality={object => {object.rotation.y += .01}} idForObject='rock' />
-			</div>
-			<div className={selectorStyle}>
-				<Displayer stage={Scissor} widthAndHeightArray={[window.innerWidth * 0.3, window.innerHeight * .3]} extraFunctionality={object => {object.rotation.y += .01}} idForObject='scissor' />
-			</div>
+			<ItemChoice id='paper' stage={Paper} />
+			<ItemChoice id='rock' stage={Rock} />
+			<ItemChoice id='scissor' stage={Scissor} />
 		</div>
 	</section>
 
