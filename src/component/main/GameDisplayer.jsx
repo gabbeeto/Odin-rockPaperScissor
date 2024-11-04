608d0cf2bef25a587138f8a12311e3fab8c8a568
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2024-11-04 14:21:55 -0300

    make scissor

--- src/component/main/GameDisplayer.jsx
+++ src/component/main/GameDisplayer.jsx
@@ -1,6 +1,4 @@
-
 import CloudStage from './../../objectsIn3D/stage.glb'
-
 import Displayer from "../display3DObjects/DisplayerFor3D.jsx"
 
 let rotation = 0
@@ -21,10 +19,7 @@ function RotateToTheRightAndComeBack() {
 
 export default function MainElement() {
 	return <section className='flex justify-center items-center'>
-
-
 		<Displayer stage={CloudStage} widthAndHeightArray={[window.innerWidth * .8, window.innerHeight * .8]} extraFunctionality={object => {RotateToTheRightAndComeBack(); object.rotation.y = rotation}} idForObject='stage1' />
-
 	</section>
 
 }
